69f5a797621012bfb8d01470f1333bbb7bf5929e
dnickinteractive <dnickinteractive@gmail.com>
2018-04-16 11:27:03 -0400

    Updated Gulp Tasks

---
+++ docs/references.md
@@ -0,0 +1,10 @@
+# References:
+
+Almost every project don't come from nowhere. We often takes ideas an insights from others. For me is useful to have a place where I can find all things that help me to develop a project. In this way I have a clear understanding about where the things come from everytime I need.
+
+This file was created to give you an access point to references related to your project. The items you actually see here was used in the development of "Nicko". This is just to share with you where things used in this project come from.
+
+
+https://www.toptal.com/javascript/optimize-js-and-css-with-gulp
+This article explains how to create a gulp task that concatenate and minify js/css files. Also create a task that make this files cache busting.
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,250 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const pathbuild = require('path');
+const sequence = require('run-sequence');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rev = require('gulp-rev');
+const delOrg = require('gulp-rev-delete-original');
+const del = require('del');
+const revReplace = require('gulp-rev-replace');
+const htmlmin = require('gulp-htmlmin');
+const pump = require('pump');
+
+// Configurations 
+// ----------------------------------------------------------------------
+
+const config = {
+	path: {
+		source: 'src',
+		target: 'dist',
+		srcStyles: 'sass',
+		distStyles: 'css',
+		images: 'imgs',
+		scripts: 'js',
+		maps: 'maps',
+		revisions: 'rev'
+	},
+	autoprefixer: {
+		browsers: ['last 4 versions']
+	},
+	sassOutput: {
+		outputStyle: 'expanded'
+		// Options: nested, expanded, compact, compressed
+		// Use compressed as default
+	},
+	images: {
+		gifsicle: { interlaced: true },
+		jpegtran: { progressive: true },
+		optipng: { optimizationLevel: 5 },
+		svgo: {
+			plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
+    	}
+	},
+	htmlmin: {
+		collapseWhitespace: true
+	}
+};
+
+
+// Paths
+// ---------------------------------------------------------------------- 
+
+const srcSassFiles = pathbuild.join(config.path.source, config.path.srcStyles, '**', '*.scss'),
+	  srcJsFiles = pathbuild.join(config.path.source, config.path.scripts, '*.js'),
+	  srcImages = pathbuild.join(config.path.source, config.path.images, '*'),
+	  srcHtml = pathbuild.join(config.path.source, '**', '*.html'),
+	  distCssFolder = pathbuild.join(config.path.target, config.path.distStyles),
+	  distCssFiles = pathbuild.join(config.path.target, config.path.distStyles, '*.css'),
+	  distJsFolder = pathbuild.join(config.path.target, config.path.scripts),
+	  distJsFiles = pathbuild.join(config.path.target, config.path.scripts, '*.js'),
+	  distImagesFolder = pathbuild.join(config.path.target, config.path.images),
+	  distImages = pathbuild.join(config.path.target, config.path.images, '*'),
+	  distHtml = pathbuild.join(config.path.target, '**', '*.html'),
+	  jsMapFiles = pathbuild.join(distJsFolder, config.path.maps, '*'),
+	  cssMapFiles = pathbuild.join(distCssFolder, config.path.maps, '*'),
+	  manifestBase = pathbuild.join(config.path.source, config.path.revisions),
+	  manifestPath = pathbuild.join(config.path.source, config.path.revisions, 'rev-manifest.json');
+
+
+// Styles 
+// ---------------------------------------------------------------------- 
 
 gulp.task('css', function() {
-	return gulp.src('src/sass/**/*.scss')
+	return gulp
+		.src(srcSassFiles)
 		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-		.pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
+		.pipe(sass(config.sassOutput).on('error', sass.logError))
+		.pipe(autoprefixer(config.autoprefixer))
         .pipe(sourcemaps.write('./maps'))
-		.pipe(gulp.dest('dist/css'))
+		.pipe(gulp.dest(distCssFolder))
 		.pipe(browserSync.stream())
 });
 
-gulp.task('images', function(){
-	return gulp.src('src/images/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('dist/images'))
+gulp.task('css-rev', function() {
+	return gulp
+		.src(srcSassFiles)
+		.pipe(sourcemaps.init())
+		.pipe(sass(config.sassOutput).on('error', sass.logError))
+		.pipe(autoprefixer(config.autoprefixer))
+		.pipe(rev())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(delOrg())
+		.pipe(gulp.dest(distCssFolder))
+		.pipe(rev.manifest({ path: manifestPath, base: manifestBase, merge: true }))
+		.pipe(gulp.dest(manifestBase))
 });
+
+
+// Scripts
+// ----------------------------------------------------------------------
+
+gulp.task('scripts', function () {
+	pump([
+		gulp.src(srcJsFiles),
+		sourcemaps.init(),
+		concat('scripts.js'),
+		uglify(),
+		sourcemaps.write('./maps'),
+		gulp.dest(distJsFolder)
+    ]);
+});
+
+gulp.task('scripts-rev', function () {
+	pump([
+		gulp.src(srcJsFiles),
+		sourcemaps.init(),
+		concat('scripts.js'),
+		uglify(),
+		rev(),
+		sourcemaps.write('./maps'),
+		delOrg(),
+		gulp.dest(distJsFolder),
+		rev.manifest({ path: manifestPath, base: manifestBase, merge: true }),
+		gulp.dest(manifestBase)
+    ]);
+});
+
+
+// HTML Minify
+// ----------------------------------------------------------------------
+
+gulp.task('html', function(cb) {
+	pump([
+		gulp.src(distHtml),
+		htmlmin(config.htmlmin),
+		gulp.dest(config.path.target)
+		],
+		cb
+    )
+});
+
+// Images 
+// ----------------------------------------------------------------------
+
+const imageminConfig = [
+	imagemin.gifsicle(config.images.gifsicle),
+	imagemin.jpegtran(config.images.jpegtran),
+	imagemin.optipng(config.images.optipng),
+	imagemin.svgo(config.images.svgo)
+];
+
+gulp.task('images', function() {
+	return gulp
+		.src(srcImages)
+		.pipe(imagemin(imageminConfig))
+		.pipe(gulp.dest(distImagesFolder))
+});
+
+gulp.task('images-rev', function() {
+	return gulp
+		.src(srcImages)
+		.pipe(imagemin(imageminConfig))
+		.pipe(rev())
+		.pipe(gulp.dest(distImagesFolder))
+		.pipe(rev.manifest({ path: manifestPath, base: manifestBase, merge: true }))
+		.pipe(gulp.dest(manifestBase))
+});
+
+
+// Revision Replace 
+// ----------------------------------------------------------------------
+
+gulp.task('revreplace', function() {
+	const manifest = gulp.src(manifestPath);
+	return gulp
+		.src('dist/**/*.+(html|xml|css|map)')
+		.pipe(revReplace({ 
+			manifest: manifest,
+			replaceInExtensions: ['.html', '.xml', '.css', '.map']
+			})
+		)
+		.pipe(gulp.dest(config.path.target))
+});
+
+
+
+// Clean Files
+// ----------------------------------------------------------------------
+gulp.task('clean', function() {
+	del([ distJsFiles, distCssFiles, distImages, jsMapFiles, cssMapFiles, manifestPath ])
+});
+
+// Copy Files
+// ----------------------------------------------------------------------
 
 gulp.task('copy', function() {
-	return gulp.src('src/**/*.+(html|js)')
-		.pipe(gulp.dest('dist'))
+	return gulp
+		.src(srcHtml)
+		.pipe(gulp.dest(config.path.target))
 		.pipe(browserSync.stream())
 });
+
+gulp.task('copyinit', function() {
+	return gulp
+		.src(['src/**/*.+(html|js|png|ico|jpg|gif|txt|xml|webmanifest)', '!src/js/*.js', '!src/imgs/*'  ])
+		.pipe(gulp.dest(config.path.target))
+});
+
+
+// Browser Sync
+// ----------------------------------------------------------------------
 
 gulp.task('browserSync', function() {
 	browserSync.init({
-		server: {
-			baseDir: 'dist'
-		},
+		server: { baseDir: config.path.target },
 	})
 });
 
-gulp.task('watch', ['browserSync', 'css'], function(){
-	gulp.watch('src/sass/**/*.scss', ['css']);
-	gulp.watch('src/**/*.+(html|js)', ['copy']);
+
+// Watch 
+// ----------------------------------------------------------------------
+
+gulp.task('watch', ['browserSync', 'css', 'scripts'], function(){
+	gulp.watch(srcSassFiles, ['css']);
+	gulp.watch(srcJsFiles, ['scripts']);
+	gulp.watch(srcHtml, ['copy']);
 });
 
-// The default task (called when you run `gulp` from cli) 
-gulp.task('default', ['watch']);
+
+// Build 
+// ----------------------------------------------------------------------
+
+gulp.task('build', function(cb) {
+	sequence('clean', 'images-rev', 'copyinit', 'css-rev', 'scripts-rev', 'revreplace', 'html', cb)
+})
+
+
+// Default ----------------------------------------------------------------------
+
+gulp.task('default', ['clean', 'images', 'copyinit', 'watch']);
+
+
+
+
+
+
