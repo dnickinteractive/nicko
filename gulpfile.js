b1c256c7323b9e596c8638a2b12e62194a029fac
Newman Barreto <dnickinteractive@gmail.com>
2018-06-04 09:10:48 -0400

    Gulp file updated

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const jshint = require('gulp-jshint');
+const uglify = require('gulp-uglify-es').default;
 const rev = require('gulp-rev');
 const delOrg = require('gulp-rev-delete-original');
 const del = require('del');
@@ -55,20 +56,22 @@ const config = {
 // ---------------------------------------------------------------------- 
 
 const srcSassFiles = pathbuild.join(config.path.source, config.path.srcStyles, '**', '*.scss'),
-	  srcJsFiles = pathbuild.join(config.path.source, config.path.scripts, '*.js'),
-	  srcImages = pathbuild.join(config.path.source, config.path.images, '*'),
-	  srcHtml = pathbuild.join(config.path.source, '**', '*.html'),
-	  distCssFolder = pathbuild.join(config.path.target, config.path.distStyles),
-	  distCssFiles = pathbuild.join(config.path.target, config.path.distStyles, '*.css'),
-	  distJsFolder = pathbuild.join(config.path.target, config.path.scripts),
-	  distJsFiles = pathbuild.join(config.path.target, config.path.scripts, '*.js'),
-	  distImagesFolder = pathbuild.join(config.path.target, config.path.images),
-	  distImages = pathbuild.join(config.path.target, config.path.images, '*'),
-	  distHtml = pathbuild.join(config.path.target, '**', '*.html'),
-	  jsMapFiles = pathbuild.join(distJsFolder, config.path.maps, '*'),
-	  cssMapFiles = pathbuild.join(distCssFolder, config.path.maps, '*'),
-	  manifestBase = pathbuild.join(config.path.source, config.path.revisions),
-	  manifestPath = pathbuild.join(config.path.source, config.path.revisions, 'rev-manifest.json');
+	    srcJsFiles = pathbuild.join(config.path.source, config.path.scripts, '*.js'),
+      srcJsPlugins = pathbuild.join(config.path.source, config.path.scripts, 'plugins.js'),
+      srcJsMain = pathbuild.join(config.path.source, config.path.scripts, 'main.js'),
+      srcImages = pathbuild.join(config.path.source, config.path.images, '*'),
+      srcHtml = pathbuild.join(config.path.source, '**', '*.html'),
+      distCssFolder = pathbuild.join(config.path.target, config.path.distStyles),
+      distCssFiles = pathbuild.join(config.path.target, config.path.distStyles, '*.css'),
+      distJsFolder = pathbuild.join(config.path.target, config.path.scripts),
+      distJsFiles = pathbuild.join(config.path.target, config.path.scripts, '*.js'),
+      distImagesFolder = pathbuild.join(config.path.target, config.path.images),
+      distImages = pathbuild.join(config.path.target, config.path.images, '*'),
+      distHtml = pathbuild.join(config.path.target, '**', '*.html'),
+      jsMapFiles = pathbuild.join(distJsFolder, config.path.maps, '*'),
+      cssMapFiles = pathbuild.join(distCssFolder, config.path.maps, '*'),
+      manifestBase = pathbuild.join(config.path.source, config.path.revisions),
+      manifestPath = pathbuild.join(config.path.source, config.path.revisions, 'rev-manifest.json');
 
 
 // Styles 
@@ -104,19 +107,45 @@ gulp.task('css-rev', function() {
 // ----------------------------------------------------------------------
 
 gulp.task('scripts', function () {
+  return gulp
+    .src( [srcJsFiles, srcJsPlugins, srcJsMain] )
+		.pipe(sourcemaps.init())
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('scripts.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(gulp.dest(distJsFolder))
+
+/*
 	pump([
-		gulp.src(srcJsFiles),
+		gulp.src( [srcJsPlugins, srcJsMain, srcJsFiles] ),
 		sourcemaps.init(),
+		jshint(),
+		jshint.reporter('default'),
 		concat('scripts.js'),
 		uglify(),
 		sourcemaps.write('./maps'),
 		gulp.dest(distJsFolder)
     ]);
+*/
 });
 
 gulp.task('scripts-rev', function () {
+  return gulp
+    .src( [srcJsPlugins, srcJsMain, srcJsFiles] )
+		.pipe(sourcemaps.init())
+		.pipe(concat('scripts.js'))
+		.pipe(uglify())
+		.pipe(rev())
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(delOrg())
+		.pipe(gulp.dest(distJsFolder))
+		.pipe(rev.manifest({ path: manifestPath, base: manifestBase, merge: true }))
+		.pipe(gulp.dest(manifestBase))
+/*
 	pump([
-		gulp.src(srcJsFiles),
+		gulp.src( [srcJsPlugins, srcJsMain, srcJsFiles] ),
 		sourcemaps.init(),
 		concat('scripts.js'),
 		uglify(),
@@ -127,6 +156,7 @@ gulp.task('scripts-rev', function () {
 		rev.manifest({ path: manifestPath, base: manifestBase, merge: true }),
 		gulp.dest(manifestBase)
     ]);
+*/
 });
 
 
@@ -227,7 +257,7 @@ gulp.task('browserSync', function() {
 gulp.task('watch', ['browserSync', 'css', 'scripts'], function(){
 	gulp.watch(srcSassFiles, ['css']);
 	gulp.watch(srcJsFiles, ['scripts']);
-	gulp.watch(srcHtml, ['copy']);
+	gulp.watch([ srcHtml, srcImages], ['copy']);
 });
 
 
